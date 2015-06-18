86ada592b6576c1038af2217be07753a391d3a00
Helge Fredheim <helge.fredheim@bekk.no>
2015-06-18 10:10:25 +0200

    NAV-design

---
+++ pacbot.js
@@ -0,0 +1,52 @@
+/*
+ * Run "pacbot -d" in this folder to view the example
+ * site in dev mode in your browser, or build an
+ * uploadable dir by running "pacbot -b".
+ */
+
+exports.config = function() {
+
+    // Create a new config object.
+    var config = {
+        port: 1234,
+        assets: {}
+    };
+
+    config.root = "../";
+
+    // Assets are specified by type, in groups,
+    // and can be arrays of files or folders.
+    // The assets are included in the layout file.
+
+    config.assets.less = {
+        'app-less': [
+            'assets/less/app.less'
+        ],
+
+        'nav-less': [
+            'nav-design/less/nav.less'
+        ]        
+    };
+
+    // Asset groups can also be just folders or a file.
+    // Use folders if the inclusion order is arbitrary.
+    config.assets.js = {
+        'vendor': [
+            'assets/js/vendor'
+        ]
+    };
+
+
+    // Add some options for UglifyJS.
+    config.uglifyjs = {
+        output: { comments: /^!|@preserve|@license|@cc_on/i }
+    };
+
+    config.layouts = {
+        'index.html': '_layouts/index.html'
+    }
+
+    // Return the config object.
+    return config;
+
+};
